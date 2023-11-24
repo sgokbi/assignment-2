6d23c0702ac17884decf6baa4bdb503dd87d94c5
sgokbi <sagorika.tech@gmail.com>
2023-11-24 11:42:23 +0200

    some code modified for get single user

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -27,23 +27,19 @@ const getAllUsersFromDB = async () => {
 };
 
 const getSingleUserFromDB = async (userId: number) => {
-  const result = await UserModel.findOne({ userId });
+  // const result = await UserModel.findOne({ userId });
 
-  // const result = await UserModel.aggregate([
-  //   {
-  //     $project: {
-  //       _id: 0,
-  //       username: 1,
-  //       'fullName.firstName': 1,
-  //       'fullName.lastName': 1,
-  //       age: 1,
-  //       email: 1,
-  //       'address.street': 1,
-  //       'address.city': 1,
-  //       'address.country': 1,
-  //     },
-  //   },
-  // ]);
+  const result = await UserModel.aggregate([
+    { $match: { userId } },
+    {
+      $project: {
+        _id: 0,
+        'fullName._id': 0,
+        'address._id': 0,
+        orders: 0,
+      },
+    },
+  ]);
   return result;
 };
 
